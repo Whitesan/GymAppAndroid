29822a218a03e0b20f36b66a59b9a6e6f846db28
Whitesan <piotrekrek12@wp.pl>
2020-11-23 17:37:19 +0100

    add planner activity

--- app/src/main/java/com/example/myapplication/PlannerActivity.kt
+++ app/src/main/java/com/example/myapplication/PlannerActivity.kt
@@ -1,11 +1,23 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class PlannerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_planner)
+        var button = findViewById(R.id.button) as Button
+        button.setOnClickListener(object: View.OnClickListener
+        {
+            override fun onClick(v: View?) {
+                val intent =  Intent(applicationContext,MainActivity::class.java)
+                startActivity(intent)
+            }
+        })
+
     }
 }
