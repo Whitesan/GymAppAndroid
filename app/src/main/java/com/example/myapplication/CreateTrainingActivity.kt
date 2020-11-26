93b72f78fc4220accea8034fd06e8ee7b2bc0087
Bartłomiej Świstak <bartekswistak20@gmail.com>
2020-11-26 19:26:17 +0100

    added text entry listeners

--- app/src/main/java/com/example/myapplication/CreateTrainingActivity.kt
+++ app/src/main/java/com/example/myapplication/CreateTrainingActivity.kt
@@ -1,24 +1,55 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.view.View
-import android.widget.Button
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 @Suppress("DEPRECATION")
 class CreateTrainingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_training)
+//        val layout=findViewById<LinearLayout>(R.id.TrainingCreatorLayout)
+//        layout.setOnTouchListener(v,event->{
+//
+//        })
         val button = findViewById<ImageView>(R.id.backTrainingCreator)
         button.setOnClickListener{
-            val intent =  Intent(applicationContext,PlannerActivity::class.java)
+            val intent =  Intent(applicationContext, PlannerActivity::class.java)
             startActivity(intent)
         }
-    }
+        var name:String?
+        val entry = findViewById<EditText>(R.id.enterTrainingName)
+        entry.setOnFocusChangeListener{ v, focus ->
+                if(focus==false){
+                    name = entry.text.toString()
+                    entry.hideKeyboard()
+                }
+                entry.isCursorVisible = focus
 
+        }
+        entry.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == EditorInfo.IME_ACTION_DONE || keyCode == EditorInfo.IME_ACTION_SEARCH) {
+                name = entry.text.toString()
+                entry.hideKeyboard()
+                entry.isCursorVisible = false
+                return@OnKeyListener true
+            }
+            false
+        })
+
+    }
+    fun View.hideKeyboard(){
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
+    }
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
