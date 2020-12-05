383712556bc202b1050600e3e6d43b81f5781206
Whitesan <piotrekrek12@wp.pl>
2020-12-05 18:05:15 +0100

    Add exercise types 2

--- app/src/main/java/com/example/myapplication/activities/EnterExerciseNameActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/EnterExerciseNameActivity.kt
@@ -42,9 +42,9 @@ class EnterExerciseNameActivity : AppWindowActivity() {
         }
 
         val typePicker = findViewById<LinearLayout>(R.id.typePicker)
-        val but = createSeriesButton("view1")
-        val but2 = createSeriesButton("view2")
-        val but3 = createSeriesButton("view3")
+        val but = createSeriesButton("   view1111111  ")
+        val but2 = createSeriesButton("   view2222222   ")
+        val but3 = createSeriesButton("   view3333333   ")
         val but4 = createSeriesButton("view4")
         val but5 = createSeriesButton("view5")
         typePicker.addView(but)
@@ -52,21 +52,27 @@ class EnterExerciseNameActivity : AppWindowActivity() {
         typePicker.addView(but3)
         typePicker.addView(but4)
         typePicker.addView(but5)
-       // typePicker.addView(but)
+        // typePicker.addView(but)
 
     }
-    fun createSeriesButton(text : String) : Button
-    {
+
+    fun createSeriesButton(text: String): Button {
         val params: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT)
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
         params.setMargins(10, 10, 10, 10)
+
         val button = Button(this)
+
+
         button.text = text
 
         button.layoutParams = params
+        button.setBackgroundDrawable(getResources().getDrawable(R.color.light_gray));
         return button;
     }
+
     private fun setTextListener() {
         val entry = findViewById<EditText>(R.id.enterExerciseNameEntry)
         entry.setOnFocusChangeListener { v, focus ->
