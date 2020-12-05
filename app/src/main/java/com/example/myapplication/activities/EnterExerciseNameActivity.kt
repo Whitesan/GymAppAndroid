4451f5a6290e60e75e3fa5fdf974f8fd5b3d90dd
Bartłomiej Świstak <bartekswistak20@gmail.com>
2020-12-05 15:46:25 +0100

    Empty String error, code cleanup

--- app/src/main/java/com/example/myapplication/activities/EnterExerciseNameActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/EnterExerciseNameActivity.kt
@@ -1,50 +1,60 @@
 package com.example.myapplication.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.Toast
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.R
 import com.example.myapplication.exercises.Exercise
 import com.example.myapplication.exercises.Part
 
 @Suppress("DEPRECATION")
 class EnterExerciseNameActivity : AppCompatActivity() {
+    private var enteredText: String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_exercise_name)
-        val button = findViewById<ImageView>(R.id.backEnterName)
-        button.setOnClickListener{
-            val intent =  Intent(applicationContext, CreateTrainingActivity::class.java)
+
+        val backButton = findViewById<ImageView>(R.id.backEnterName)
+        backButton.setOnClickListener {
+            val intent = Intent(applicationContext, CreateTrainingActivity::class.java)
             startActivity(intent)
+
         }
-        val button2 = findViewById<Button>(R.id.ContinueCreatingButton)
-        var name:String="none"
-        button2.setOnClickListener{
-               val  exercise= Part.getPart("plecy")?.let { part -> Exercise(name, part) }
-                val intent =  Intent(applicationContext, ExerciseActivity::class.java)
-                intent.putExtra("Exercise",exercise)
+
+        setTextListener()
+
+        val buttonContinue = findViewById<Button>(R.id.ContinueCreatingButton)
+        buttonContinue.setOnClickListener {
+            if (enteredText.isEmpty()) {
+                showErrorMessage()
+            } else {
+                val exercise = Part.getPart("plecy")?.let { part -> Exercise(enteredText, part) }
+                val intent = Intent(applicationContext, ExerciseActivity::class.java)
+                intent.putExtra("Exercise", exercise)
                 startActivity(intent)
+            }
         }
-        val entry=findViewById<EditText>(R.id.enterExerciseNameEntry)
-        entry.setOnFocusChangeListener{ v, focus ->
-            if(focus==false){
-                name = entry.text.toString()
+
+    }
+
+    private fun setTextListener() {
+
+        val entry = findViewById<EditText>(R.id.enterExerciseNameEntry)
+        entry.setOnFocusChangeListener { v, focus ->
+            if (focus == false) {
+                enteredText = entry.text.toString()
                 entry.hideKeyboard()
             }
             entry.isCursorVisible = focus
-
         }
         entry.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == EditorInfo.IME_ACTION_DONE || keyCode == EditorInfo.IME_ACTION_SEARCH) {
-                name = entry.text.toString()
+                enteredText = entry.text.toString()
                 entry.hideKeyboard()
                 entry.isCursorVisible = false
                 return@OnKeyListener true
@@ -52,9 +62,14 @@ class EnterExerciseNameActivity : AppCompatActivity() {
             false
         })
 
-
-
     }
+
+    private fun showErrorMessage() {
+        val message = findViewById<TextView>(R.id.TrainingNameEmpty)
+        message.setVisibility(View.VISIBLE);
+    }
+
+
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
@@ -71,11 +86,13 @@ class EnterExerciseNameActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
+
     override fun onResume() {
         super.onResume()
         hideSystemUI()
     }
-    fun View.hideKeyboard(){
+
+    fun View.hideKeyboard() {
         hideSystemUI()
         val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
         inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
