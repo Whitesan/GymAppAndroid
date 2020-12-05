0598924f32004be291ecbd4f19406607124f8eca
Bartłomiej Świstak <bartekswistak20@gmail.com>
2020-12-05 17:12:58 +0100

    show navigation

--- app/src/main/java/com/example/myapplication/activities/CreateTrainingActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/CreateTrainingActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
+import android.view.WindowManager
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.*
@@ -75,8 +76,6 @@ class CreateTrainingActivity : AppCompatActivity() {
         })
     }
 
-
-
     fun View.hideKeyboard(){
             hideSystemUI()
             val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
@@ -90,13 +89,8 @@ class CreateTrainingActivity : AppCompatActivity() {
     }
 
     private fun hideSystemUI() {
-        val decorView: View = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
     override fun onResume() {
         super.onResume()
