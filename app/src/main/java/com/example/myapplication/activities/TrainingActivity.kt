ded58237e7a2802fa44940fceef56f9e5c5f4eed
Bartłomiej Świstak <bartekswistak20@gmail.com>
2021-01-07 17:28:55 +0100

    TODO added

--- app/src/main/java/com/example/myapplication/activities/TrainingActivity.kt
+++ app/src/main/java/com/example/myapplication/activities/TrainingActivity.kt
@@ -8,6 +8,27 @@ import android.view.WindowManager
 import android.widget.ImageView
 import com.example.myapplication.R
 
+
+
+//TODO import training list from json, according to week day
+
+/*
+TODO     layout
+1.actual part and exercise
+2.clock + start/stop button
+3.show planned reps and weight  + enter done reps and weight
+4.skip/end series (?)
+5.allow user to change expected training for another (maybe open calendar?)
+ */
+/*TODO save stats to JSON
+1.day
+2.exercise
+3.predicted and actual  reps + weight
+4.(?)time:  length of exercise and length if rest
+*/
+//TODO On finish training show summary stats and congratulations (or not)
+//TODO maybe on finish ask for user weight, if he'd like to share save that for future statistics
+
 @Suppress("DEPRECATION")
 class TrainingActivity : AppWindowActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
